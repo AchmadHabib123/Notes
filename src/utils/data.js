7dabe52580df8cc881445f9668aad80e81d78e31
AchmadHabib123 <98659014+AchmadHabib123@users.noreply.github.com>
2022-08-12 18:50:51 +0700

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -4,18 +4,21 @@ const getData = () => {
             name: 'Dimas Saputra',
             tag: 'dimasmds',
             imageUrl: '/images/dimasmds.jpeg',
+            archived: false,
         },
         {
             id: 2,
             name: 'Arif Faizin',
             tag: 'arifaizin',
             imageUrl: '/images/arifaizin.jpeg',
+            archived: false,
         },
         {
             id: 3,
             name: 'Rahmat Fajri',
             tag: 'rfajri27',
             imageUrl: '/images/rfajri27.jpeg',
+            archived: false,
         },
     ];
 }
